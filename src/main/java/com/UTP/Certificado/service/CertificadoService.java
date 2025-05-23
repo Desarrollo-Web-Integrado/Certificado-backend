7132e5fcacbf84538202199ae9ba81250b2a197f
Fernando Raul <prueba@correo.com>
2025-05-23 17:08:26 -0500

    core: Agregar las bases de la capa de servicio

---
+++ src/main/java/com/UTP/Certificado/service/CertificadoService.java
@@ -0,0 +1,31 @@
+package com.UTP.Certificado.service;
+
+import com.UTP.Certificado.dto.CertificadoRequestDTO;
+import com.UTP.Certificado.model.Certificado;
+import com.UTP.Certificado.repository.CertificadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CertificadoService {
+
+    @Autowired
+    private CertificadoRepository certificadoRepository;
+
+    public Certificado crearCertificado(CertificadoRequestDTO dto) {
+        Certificado certificado = new Certificado();
+        certificado.setNombreEstudiante(dto.getNombreEstudiante());
+        certificado.setCurso(dto.getCurso());
+        certificado.setNota(dto.getNota());
+        certificado.setFechaEmision(dto.getFechaEmision());
+        certificado.setCodigoVerificacion(generarCodigoUnico());
+
+        return certificadoRepository.save(certificado);
+    }
+
+    private String generarCodigoUnico() {
+        return UUID.randomUUID().toString(); // Sirve como código de verificación posible QR
+    }
+}
