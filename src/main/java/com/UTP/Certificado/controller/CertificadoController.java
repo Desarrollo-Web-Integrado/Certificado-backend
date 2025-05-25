3c29ef44068a8e9652db98c7613b589546121c81
Fernando Raul <prueba@correo.com>
2025-05-24 23:26:20 -0500

    core: Agregar endpoint para descargar certificado en PDF

--- src/main/java/com/UTP/Certificado/controller/CertificadoController.java
+++ src/main/java/com/UTP/Certificado/controller/CertificadoController.java
@@ -3,13 +3,12 @@ package com.UTP.Certificado.controller;
 
 import com.UTP.Certificado.dto.CertificadoRequestDTO;
 import com.UTP.Certificado.model.Certificado;
+import com.UTP.Certificado.repository.CertificadoRepository;
 import com.UTP.Certificado.service.CertificadoService;
+import com.UTP.Certificado.service.PdfGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/certificados")
@@ -18,9 +17,33 @@ public class CertificadoController {
     @Autowired
     private CertificadoService certificadoService;
 
+    @Autowired
+    private PdfGeneratorService pdfGeneratorService;
+
+    @Autowired
+    private CertificadoRepository certificadoRepository;
+
+
+
+
     @PostMapping
     public ResponseEntity<Certificado> crearCertificado(@RequestBody CertificadoRequestDTO dto) {
         Certificado nuevoCertificado = certificadoService.crearCertificado(dto);
         return ResponseEntity.ok(nuevoCertificado);
     }
+
+
+    @GetMapping("/{id}/pdf")
+    public ResponseEntity<byte[]> descargarCertificadoPdf(@PathVariable Long id) {
+        Certificado certificado = certificadoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Certificado no encontrado"));
+
+        byte[] pdfBytes = pdfGeneratorService.generarPdfCertificado(certificado);
+
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=certificado_" + id + ".pdf")
+                .contentType(org.springframework.http.MediaType.APPLICATION_PDF)
+                .body(pdfBytes);
+    }
+
 }
