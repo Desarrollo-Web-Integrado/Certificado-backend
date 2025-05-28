cc103970a14e2caed193d79e9a45614c9c3e3e0b
Fernando Raul <prueba@correo.com>
2025-05-27 20:34:55 -0500

    core: añadir cors

---
+++ src/main/java/com/UTP/Certificado/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.UTP.Certificado.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // aplica a todos los endpoints
+                        .allowedOrigins("http://localhost:4321") // frontend de Astro
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // métodos permitidos
+                        .allowedHeaders("*") // permite todos los headers
+                        .allowCredentials(true); // si usas cookies o Authorization
+            }
+        };
+    }
+}
