57b62789b5422311b9ad668197a794d12557d591
Fernando Raul <prueba@correo.com>
2025-06-26 15:47:07 -0500

    core: reajuste de codigo, falta agregar manualmente getter, setters y contructores

--- src/main/java/com/UTP/Certificado/dto/CertificadoRequestDTO.java
+++ src/main/java/com/UTP/Certificado/dto/CertificadoRequestDTO.java
@@ -1,76 +1,21 @@
 package com.UTP.Certificado.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 public class CertificadoRequestDTO {
 
-    private String nombreEstudiante;
-    private String codigo;
+    private String correo; // ✅ Buscar al estudiante por su correo único
+
     private String curso;
     private Double nota;
     private LocalDate fechaEmision;
 
-    //NUEVOS CAMPOS
     private List<String> habilidades;
     private String descripcion;
-
-    //getters y setters
-
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public List<String> getHabilidades() {
-        return habilidades;
-    }
-
-    public void setHabilidades(List<String> habilidades) {
-        this.habilidades = habilidades;
-    }
-
-    public String getNombreEstudiante() {
-        return nombreEstudiante;
-    }
-
-    public void setNombreEstudiante(String nombreEstudiante) {
-        this.nombreEstudiante = nombreEstudiante;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
-
-    public Double getNota() {
-        return nota;
-    }
-
-    public void setNota(Double nota) {
-        this.nota = nota;
-    }
-
-    public LocalDate getFechaEmision() {
-        return fechaEmision;
-    }
-
-    public void setFechaEmision(LocalDate fechaEmision) {
-        this.fechaEmision = fechaEmision;
-    }
 }
