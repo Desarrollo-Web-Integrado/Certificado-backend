4c0551f3bee2f16ba771365edda8a1b55139cef9
Fernando Raul <prueba@correo.com>
2025-06-26 15:48:01 -0500

    core: reajuste de codigo, falta agregar manualmente getter, setters y contructores

--- src/main/java/com/UTP/Certificado/model/Usuario.java
+++ src/main/java/com/UTP/Certificado/model/Usuario.java
@@ -1,66 +1,28 @@
 package com.UTP.Certificado.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Usuario {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
     private String apellido;
+
+    @Column(unique = true, nullable = false)
     private String correo;
+
     private String clave;
 
     @Enumerated(EnumType.STRING)
     private Rol rol;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
 }
