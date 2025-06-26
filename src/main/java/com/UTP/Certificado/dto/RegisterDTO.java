00451f43cb150f93b14a1d2187263a110a4fa364
Fernando Raul <prueba@correo.com>
2025-06-26 15:48:17 -0500

    core: reajuste de codigo, falta agregar manualmente getter, setters y contructores

--- src/main/java/com/UTP/Certificado/dto/RegisterDTO.java
+++ src/main/java/com/UTP/Certificado/dto/RegisterDTO.java
@@ -2,51 +2,26 @@ package com.UTP.Certificado.dto;
 
 import com.UTP.Certificado.model.Rol;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RegisterDTO {
     private String nombre;
     private String apellido;
     private String correo;
     private String clave;
-    private String rol; // luego se convierte a Enum
+    private String rol;
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-
+    // ✅ Método para convertir el string a Enum Rol
     public Rol getRolEnum() {
-        return Rol.valueOf(this.rol.toUpperCase());
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
+        try {
+            return Rol.valueOf(rol.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Rol inválido. Usa ADMINISTRADOR o USER.");
+        }
     }
 }
 
