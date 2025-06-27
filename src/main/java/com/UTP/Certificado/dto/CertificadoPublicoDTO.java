9eb14aa87cf30cc1f74f296c22e212598e10e20b
Fernando Raul <prueba@correo.com>
2025-06-27 14:10:41 -0500

    core: agregar DTO de respuesta para el certificado

---
+++ src/main/java/com/UTP/Certificado/dto/CertificadoPublicoDTO.java
@@ -0,0 +1,21 @@
+package com.UTP.Certificado.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CertificadoPublicoDTO {
+
+    private String nombreCompleto;
+    private String curso;
+    private Double nota;
+    private String descripcion;
+    private List<String> habilidades;
+    private LocalDate fechaEmision;
+}
